8a09dd6fac1df464496d76d6d1bd54291872b5f9
Md Istiaq Ahmed <istiaqahmed121998@gmail.com>
2022-07-29 07:18:20 +0600

    view added and translator added

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const i18n = require('i18n-express');
 const { connectWithRetry: dbconnection } = require('./db/connection');
 
 const app = express();
-const port = process.env.PORT || 8000;
-
+app.use(
+    i18n({
+        translationsPath: path.join(__dirname, 'i18n'), // <--- use here. Specify translations files path.
+        siteLangs: ['es', 'en', 'de', 'ru', 'it', 'fr'],
+        textsVarName: 'translation',
+    }),
+);
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 app.get('/', (req, res) => {
-    res.send('Hello World! ');
+    res.locals = {
+        title: 'Dashboard',
+    };
+    res.render('Dashboard/index');
 });
 
-app.set('view engine', 'ejs');
-
+const port = process.env.PORT || 8000;
 dbconnection();
 
 app.listen(port, () => {
